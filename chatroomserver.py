11193d3207bbc8f151a6db742c0ea92e52bc3351
Nilabh Sahu <59756708+nilabh22@users.noreply.github.com>
2021-10-03 15:25:43 +0530

    Add files via upload

---
+++ chatroomserver.py
@@ -0,0 +1,27 @@
+import socket
+import time
+import sys
+
+# create a socket object
+serversocket = socket.socket(
+	        socket.AF_INET, socket.SOCK_STREAM)
+
+# get local machine name
+host = socket.gethostname()
+
+port = 4444
+
+# bind to the port
+serversocket.bind((host, port))
+
+# queue up to 5 requests
+serversocket.listen(5)
+
+while True:
+    # establish a connection
+    clientsocket,addr = serversocket.accept()
+
+    print("Got a connection from %s" % str(addr))
+    currentTime = time.ctime(time.time()) + "\r\n"
+    clientsocket.send(currentTime.encode('ascii'))
+    clientsocket.close()
